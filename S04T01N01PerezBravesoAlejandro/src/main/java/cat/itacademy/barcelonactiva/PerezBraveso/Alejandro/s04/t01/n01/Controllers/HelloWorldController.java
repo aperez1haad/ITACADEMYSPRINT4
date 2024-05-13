4018a6c4702f22e85f0ac7bc55d925adad91f9b4
aperez1haad <159771117+aperez1haad@users.noreply.github.com>
2024-05-13 12:36:59 +0200

    Add files via upload

--- S04T01N01PerezBravesoAlejandro/src/main/java/cat/itacademy/barcelonactiva/PerezBraveso/Alejandro/s04/t01/n01/Controllers/HelloWorldController.java
+++ S04T01N01PerezBravesoAlejandro/src/main/java/cat/itacademy/barcelonactiva/PerezBraveso/Alejandro/s04/t01/n01/Controllers/HelloWorldController.java
@@ -1,6 +1,6 @@
 package cat.itacademy.barcelonactiva.PerezBraveso.Alejandro.s04.t01.n01.Controllers;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.Optional;
 @RestController
     public class HelloWorldController {
 
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.*;
         public String saluda(@RequestParam(name = "nom", defaultValue = "UNKNOWN") String nom) {
             return "Hola, " + nom + ". Estàs executant un projecte Maven";
         }
+    /*
+    @GetMapping("/HelloWorld2/{nom}")
+    public String saluda2(@PathVariable(Optional<String> nom) {
+        return "Hola, " + nom.orElse("UNKNOWN") + ". Estàs executant un projecte Maven";
+    }*/
 
         @GetMapping("/HelloWorld2/{nom}")
         public String saluda2(@PathVariable(name = "nom", required = false) String nom) {
